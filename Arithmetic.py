79a3d8d1f292422cc48f5a712e630e9cc684c761
Nikkoxxx <151499461+Nikkoxxx@users.noreply.github.com>
2023-11-20 14:40:39 +0800

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,23 @@
+#Exercise 2 - Arithmetic Operations
+
+num1 = 32
+num2 = 41
+
+#Addition
+sum = num1 + num2 
+
+#Subtraction
+diff = num1 - num2 
+
+#Multiplication
+product = num1 * num2
+
+#Division
+quotient = num1 / num2
+
+
+#Result
+print("The sum is",num1, "and", num2, "is", sum)
+print("The diff is",num1, "and", num2, "is", diff)
+print("The product is",num1, "and", num2, "is", product)
+print("The quotient is",num1, "and", num2, "is", quotient)
